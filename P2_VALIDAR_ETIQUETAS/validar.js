ce6407ea913acd3ea08f4fc1b2d23ba87e7a3d20
Emilio Noyola <noyola.gomez.emilio.damian@gmail.com>
2025-02-24 20:03:04 -0600

    Actualizo Práctica 2. Validar Etiquetas

--- P2_VALIDAR_ETIQUETAS/validar.js
+++ P2_VALIDAR_ETIQUETAS/validar.js
@@ -1,29 +1,29 @@
 function sanitizarInput(input) {
-    // Expresión regular para eliminar etiquetas <script> y su contenido
-    return input.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '');
+    return input.replace(/<[^>]+>/g, '');
 }
 
-function validar(form) {
+function validar() {
+    var form = document.forms["registro"];
+
     // Validar y sanitizar nombre
     var nombre = form.nombre.value.trim();
-    if (nombre === "") {
-        alert("Por favor, ingrese su nombre.");
-        return false;
+    var nombreSanitizado = sanitizarInput(nombre);
+
+    if (nombre !== nombreSanitizado) {
+        alert("El nombre contiene contenido no permitido.");
+        return false; 
     }
-    // Sanitizar el nombre
-    nombre = sanitizarInput(nombre);
-    if (nombre !== form.nombre.value.trim()) {
-        alert("El nombre contiene contenido no permitido y ha sido sanitizado.");
-    }
-    form.nombre.value = nombre; // Actualizar el valor del campo con el nombre sanitizado
+
+    alert(nombreSanitizado);
 
     // Validar edad
     var edad = form.edad.value.trim();
     if (edad === "" || isNaN(edad) || edad <= 0) {
         alert("Por favor, ingrese una edad válida.");
-        return false;
+        return false; 
     }
 
-    // Si todo es válido
-    return true;
+    alert(edad);
+
+    return true; 
 }
